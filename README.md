0abba6ea9d82bfc26b7a99d1eb3771ae1a25cd15
YW Chang <andy.yu29@gmail.com>
2022-12-22 14:33:47 +0800

    Modify the issue of Pyinstaller in version 5.7.0

--- README.md
+++ README.md
@@ -17,6 +17,7 @@
     ```shell
     python scripts/build_exec.py
     ```
+    - [Install Windows SDK](https://developer.microsoft.com/zh-tw/windows/downloads/windows-sdk/)
 ## 功能 Functions
 - 搜尋`三大法人買賣金額統計表`、`三大法人買賣超日報`、`三大法人買賣超週報`、`三大法人買賣超月報`\
     :notes: `年`請輸入西元 (yyyy)，`週`請輸入該週的星期一 (Week start from  Monday)
--- stock_searcher/__main__.py
+++ stock_searcher/__main__.py
@@ -1,3 +1,11 @@
 from stock_searcher import run
+import sys
+import os
+
 if __name__ == "__main__":
+    # 修正 PyInstaller v5.7.0 問題 https://github.com/pyinstaller/pyinstaller/issues/7325
+    if sys.stdout is None:
+        sys.stdout = open(os.devnull, "w")
+    if sys.stderr is None:
+        sys.stderr = open(os.devnull, "w")
     run()
