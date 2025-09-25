4ece99085d997f818505b0b7537535137386dddd
YW0330 <andy.yu29@gmail.com>
2025-09-26 01:23:28 +0800

    Fix PyInstaller packaged issue for pip_system_certs

--- stock_searcher/__main__.py
+++ stock_searcher/__main__.py
@@ -1,9 +1,11 @@
+# Fix PyInstaller packaged pip_system_certs https://gitlab.com/alelec/pip-system-certs
+import pip_system_certs.wrapt_requests; pip_system_certs.wrapt_requests.inject_truststore()
 from stock_searcher import run
 import sys
 import os
 
 if __name__ == "__main__":
-    # 修正 PyInstaller v5.7.0 問題 https://github.com/pyinstaller/pyinstaller/issues/7325
+    # Fix PyInstaller v5.7.0 issue https://github.com/pyinstaller/pyinstaller/issues/7325
     if sys.stdout is None:
         sys.stdout = open(os.devnull, "w")
     if sys.stderr is None:
